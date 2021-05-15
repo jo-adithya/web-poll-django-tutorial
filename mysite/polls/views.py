62bcf5492a275eab06de905351e6cefdb6a5eb33
Jonathan Adithya <jo.adithya391@gmail.com>
2021-05-15 17:21:20 +0700

    create template for detail in polls

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,15 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Question
 
 # Create your views here.
 def index(request):
     latest_questions = Question.objects.order_by('-pub_date')[:5]
-    context = { 'latest_questions': latest_questions, }
-    return render(request, 'polls/index.html', context=context)
+    return render(request, 'polls/index.html', { 'latest_questions': latest_questions, })
 
 def detail(request, question_id):
-    return HttpResponse(f'You are looking at question {question_id}')
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', { 'question': question })
 
 def results(request, question_id):
     return HttpResponse(f'You are looking at the results of question {question_id}')
